4a7f9a9cf864df571913b36218d05a341d492933
ElodieLongo <mail.elodielongo@gmail.com>
2024-02-07 17:20:06 +0100

    01-WIP

--- queries/04-where-clause.sql
+++ queries/04-where-clause.sql
@@ -1,1 +1,4 @@
 -- Write a query that selects all albums made after 2010
+-- to do
+SELECT *
+FROM albums limit realease_year>2010;
--- queries/06-insert.sql
+++ queries/06-insert.sql
@@ -1,2 +1,3 @@
 -- Add Track #13 titled "Endless, Nameless" to Nirvana's Nevermind album
 -- Nirvana's id is 6, Nevermind's id is 9
+insert INTO songs (name) values ("Endless, Nameless")
